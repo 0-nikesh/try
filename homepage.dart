bc5572ca2d8364ad17065b8bc26f7dfb7f9f6437
0-nikesh <108478882+0-nikesh@users.noreply.github.com>
2023-10-09 20:03:25 +0545

    NavBar done☺☺

---
+++ homepage.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class homepage extends StatefulWidget {
+  const homepage({super.key});
+
+  @override
+  State<homepage> createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
+  int _selectedIndex = 3;
+  void _navigateBottomNavBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("body here")),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _navigateBottomNavBar,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.video_call), label: "Reel"),
+            BottomNavigationBarItem(icon: Icon(Icons.shop), label: "Shop"),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Account")
+          ]),
+    );
+  }
+}
